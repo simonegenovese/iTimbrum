cc56b3e147628d750767480d355342cce4ee1324
simonegenovese <genovese@esteco.com>
2014-10-01 16:40:23 +0200

    Aggiunto fencing per uscita ed entrata

--- KitLocateDelegateClass.m
+++ KitLocateDelegateClass.m
@@ -21,28 +21,39 @@
     NSUserDefaults * standardUserDefaults = [NSUserDefaults standardUserDefaults];
     NSNumber * latitudine = [standardUserDefaults objectForKey:@"latitudine_preference"];
     NSNumber * longitudine = [standardUserDefaults objectForKey:@"longitudine_preference"];
-    KLGeofence *myGeofence = [KLGeofence createNewGeofenceWithLatitude:[latitudine floatValue] Longitude:[longitudine floatValue] PushRadius:20.0 Type:KL_GEOFENCE_TYPE_IN];
-    [myGeofence setIDUser:@"Lavoro"];
-    [KLLocation addGeofence:myGeofence];
+    KLGeofence *inmyGeofence = [KLGeofence createNewGeofenceWithLatitude:[latitudine floatValue] Longitude:[longitudine floatValue] PushRadius:20.0 Type:KL_GEOFENCE_TYPE_IN];
+    
+    [inmyGeofence setIDUser:@"Lavoro"];
+    [KLLocation addGeofence:inmyGeofence];
+    
+    KLGeofence *outmyGeofence = [KLGeofence createNewGeofenceWithLatitude:[latitudine floatValue] Longitude:[longitudine floatValue] PushRadius:20.0 Type:KL_GEOFENCE_TYPE_OUT];
+    
+    [outmyGeofence setIDUser:@"Lavoro"];
+    [KLLocation addGeofence:outmyGeofence];
 }
 
 - (void)geofencesIn:(NSArray*)arrGeofenceList {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"GeoFencing"
-                                                    message:@"In"
-                                                   delegate:self
-                                          cancelButtonTitle:@"Annulla"
-                                          otherButtonTitles:@"Ok", nil];
-    [alert show];
+    UILocalNotification *scheduledAlert;
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    scheduledAlert = [[UILocalNotification alloc] init];
+    scheduledAlert.applicationIconBadgeNumber=1;
+    scheduledAlert.fireDate = nil;
+    scheduledAlert.timeZone = [NSTimeZone defaultTimeZone];
+    scheduledAlert.alertBody = @"Ben arrivato a lavoro, ricorda di timbrare.";
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:scheduledAlert];
 
 }
 
 - (void)geofencesOut:(NSArray*)arrGeofenceList{
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Geofencing"
-                                                    message:@"Out"
-                                                   delegate:self
-                                          cancelButtonTitle:@"Annulla"
-                                          otherButtonTitles:@"Ok", nil];
-    [alert show];
-
+    UILocalNotification *scheduledAlert;
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    scheduledAlert = [[UILocalNotification alloc] init];
+    scheduledAlert.applicationIconBadgeNumber=1;
+    scheduledAlert.fireDate = nil;
+    scheduledAlert.timeZone = [NSTimeZone defaultTimeZone];
+    scheduledAlert.alertBody = @"Stai andando via? Ricorda di timbrare l'uscita";
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:scheduledAlert];
 }
 @end
